2908e486a0a83353418ac2e43290e0319c6bd5cc
MecHR17 <asrin17cankaya@hotmail.com>
2023-12-12 13:31:09 +0300

    akjsdhksa

--- lib/myAppBar.dart
+++ lib/myAppBar.dart
@@ -1,3 +1,3 @@
 import 'package:flutter/material.dart';
 
-var myAppBar = AppBar(title: const Text("Slm"),backgroundColor: const Color(0xEE04364A),);
+var myAppBar = AppBar(title: const Text("Blog",style: TextStyle(color: Colors.white),),backgroundColor: const Color(0xEE04364A),);
